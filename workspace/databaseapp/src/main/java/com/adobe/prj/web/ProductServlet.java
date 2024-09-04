6c1ec4b8956e518ab485079f9bd525f215cea0f8
Banuprakash <banuprakashc@yahoo.co.in>
2024-09-04 12:46:27 +0530

    day 3

---
+++ workspace/databaseapp/src/main/java/com/adobe/prj/web/ProductServlet.java
@@ -0,0 +1,42 @@
+package com.adobe.prj.web;
+
+import com.adobe.prj.dao.ProductDao;
+import com.adobe.prj.dao.ProductDaoJdbcImpl;
+import com.adobe.prj.entity.Product;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/products")
+public class ProductServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html"); //MIME image/gif image/png text/js text/css application/msword
+        // open stream to the client <<Browser>>
+        PrintWriter out = resp.getWriter(); // character stream
+        out.print("<html>");
+        out.print("<body>");
+        out.print("<table border=\"1\">");
+            out.print("<tr><th>ID</th><th>Name</th><th>Price</th></tr>");
+        ProductDao productDao = new ProductDaoJdbcImpl();
+        List<Product> products = productDao.getProducts();
+        for(Product p : products) {
+            out.print("<tr>");
+                out.print("<td>" + p.getId() + "</td>");
+                out.print("<td>" + p.getName() + "</td>");
+                out.print("<td>" + p.getPrice() + "</td>");
+            out.print("</tr>");
+        }
+        out.print("</table>");
+        out.print("<a href=\"index.html\">Back </a>");
+        out.print("</body>");
+        out.print("</html>");
+    }
+}
