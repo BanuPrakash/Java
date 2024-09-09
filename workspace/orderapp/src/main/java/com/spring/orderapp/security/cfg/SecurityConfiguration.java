6ff8ed4e050e0042e7aa3e870f0dbfd8a9b9e5de
Banuprakash <banuprakashc@yahoo.co.in>
2024-09-09 14:58:49 +0530

    security

---
+++ workspace/orderapp/src/main/java/com/spring/orderapp/security/cfg/SecurityConfiguration.java
@@ -0,0 +1,58 @@
+package com.spring.orderapp.security.cfg;
+
+import com.spring.orderapp.security.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final UserDetailsServiceImpl userService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // http://localhost:8080/auth/register
+    // http://localhost:8080/auth/login
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity security) throws Exception {
+        security.authorizeHttpRequests(requests -> requests.requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/api/orders/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .sessionManagement(mgmt -> mgmt.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return security.build();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService.userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration cfg) throws Exception{
+        return cfg.getAuthenticationManager();
+    }
+}
